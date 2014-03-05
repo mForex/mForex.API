ff45408ef688ff5b6554200c255994bc900ada36
Wojciech Stokowiec <wojciech.stokowiec@hotmail.com>
2014-03-05 14:40:08 +0100

    First sample has been added

---
+++ Samples/Console Trader 0.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using mForex.API;
+
+namespace Console_Trader_0._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var client = new APIClient(new APIConnection(ServerType.Demo));
+
+            client.Connect().Wait();
+            Console.WriteLine("Connected");
+
+            client.Login(77000005, "Haslo123").ContinueWith(r =>
+            {
+                var resp = r.Result;
+                Console.WriteLine("Login response: {0} - {1}", resp.Login, resp.LoggedIn);
+            });
+
+            client.Ticks += ticks =>
+            {
+                foreach (var tick in ticks)
+                    if(tick.Symbol == "EURUSD")
+                        Console.WriteLine("{0} \t {1}/{2} \t ({3})", tick.Symbol, tick.Bid, tick.Ask, tick.Time);
+            };
+
+            while (Console.ReadKey().Key != ConsoleKey.Escape)
+            {
+
+            }
+        }
+    }
+}
